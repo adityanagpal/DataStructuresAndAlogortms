bb92f587f39f7aaa2220291130c8e54f0fd9c521
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-06 10:15:58 +0530

    Create IndexMapping.java

---
+++ Arrays/IndexMapping.java
@@ -0,0 +1,8 @@
+/*
+Given a limited range array contains both positive and non positive numbers, i.e., elements are in range 
+from -MAX to +MAX. Our task is to search if some number is present in the array or not in O(1) time.
+*/
+class IndexMapping
+{
+static int max=1000;
+}
